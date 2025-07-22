33fd9b8ed35a095c364cfcf6f54978849c926c1e
Abdussalam-Mujeeb-ur-rahman <mjbabdussalam@gmail.com>
2025-07-22 01:32:32 +0100

    scroll behaviour done automatically

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 // import Navbar from "./components/Navbar.jsx";
 
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+      const el = document.getElementById(id);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [location]);
   return (
     <div>
       {/* <Navbar /> */}
